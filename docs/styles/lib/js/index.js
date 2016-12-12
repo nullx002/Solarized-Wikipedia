f49badca3b61d4a78100a71d96a75617eb82f021
nullx002 <nullx002@users.noreply.github.com>
2016-12-12 22:57:20 +0530

    Add files via upload

---
+++ docs/styles/lib/js/index.js
@@ -0,0 +1,14 @@
+var headertext = [],
+headers = document.querySelectorAll("th"),
+tablerows = document.querySelectorAll("th"),
+tablebody = document.querySelector("tbody");
+
+for(var i = 0; i < headers.length; i++) {
+  var current = headers[i];
+  headertext.push(current.textContent.replace(/\r?\n|\r/,""));
+} 
+for (var i = 0, row; row = tablebody.rows[i]; i++) {
+  for (var j = 0, col; col = row.cells[j]; j++) {
+    col.setAttribute("data-th", headertext[j]);
+  } 
+}
